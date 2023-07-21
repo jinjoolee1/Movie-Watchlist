a1abbc7b83040a4ca2d46d9d43d48b24f0e6bf91
jinjoolee1 <jinjoolee126@gmail.com>
2023-07-21 13:01:36 -0500

    stopping point 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useEffect, useState} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [page, setPage] = useState(1);
+
+
+const getData = () => {
+  axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+      .then((res) => {
+          console.log(res.data.results);
+          setMovieList(res.data.results);
+      });
+};
+
+useEffect(() => {
+  getData();
+}, [page]);
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import Header from './components/Header'
+
+return (
+    <div className='App'>
+        <Header />
+    </div>
+)
